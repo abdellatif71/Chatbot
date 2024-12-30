7b51cc4c5ccd29438531d6cddde3ecf88ac8ff56
abdellatif71 <abouabdellatif.9@gmail.com>
2024-12-30 18:23:47 +0100

    update

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start the server
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
